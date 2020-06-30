bf047d8b10e1ee884368df940aa914ececf9d0d1
Og Astorga <og.asdiaz@gmail.com>
2020-06-29 20:28:37 -0500

    Receive and return []byte slices

--- src/set1/challenge5.go
+++ src/set1/challenge5.go
@@ -1,14 +1,12 @@
 package set1
 
-import "../utils"
 
-
-func RepeatingKeyXOR(data string, key string) string {
+func RepeatingKeyXOR(data []byte, key []byte) []byte {
   encrypted := make([]byte, len(data))
 
   for i:=0; i<len(data); i+=1 {
     encrypted[i] = data[i] ^ key[i%len(key)]
   }
 
-  return utils.BytesToHex(encrypted)
+  return encrypted
 }
--- src/set1/challenge5_test.go
+++ src/set1/challenge5_test.go
@@ -6,17 +6,17 @@ import "../utils"
 
 func TestChallenge5(t *testing.T) {
   input := "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+  key := []byte("ICE")
   excepted := "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
 
-  output := RepeatingKeyXOR(input, "ICE")
-  output2 := RepeatingKeyXOR(string(utils.HexToBytes(output)), "ICE")
-  ascii := utils.HexToBytes(output2)
+  output := RepeatingKeyXOR([]byte(input), key)
+  output2 := RepeatingKeyXOR(output, key)
 
-  if output != excepted {
-    t.Errorf("Excepted '%s'; got '%s'", excepted, output)
+  if utils.BytesToHex(output) != excepted {
+    t.Errorf("Excepted '%s'; got '%s'", excepted, utils.BytesToHex(output))
   }
 
-  if string(ascii) != input {
-    t.Errorf("Excepted '%s'; got '%s'", input, ascii)
+  if string(output2) != input {
+    t.Errorf("Excepted '%s'; got '%s'", input, string(output2))
   }
 }
